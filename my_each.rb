37fae8a08afc6e145441d18d0d94cdcef2b8cdb3
LitatyHQ <litatyHQ@gmail.com>
2020-10-10 23:10:58 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)   
-  x = 0    
-  while x < array.length     
-    yield(array[x])     
-    x = x + 1   
+  i = 0    
+  while i < array.length     
+    yield(array[i])     
+    i = i + 1   
   end    
   array 
 end
